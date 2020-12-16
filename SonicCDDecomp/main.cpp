57bc8643cf972e528ff75352450391b5cf2258f2
Rubberduckycooly <rubberduckycooly@outlook.com>
2020-12-16 15:16:00 +1100

    minor fix

--- SonicCDDecomp/main.cpp
+++ SonicCDDecomp/main.cpp
@@ -1,13 +1,7 @@
 #include "RetroEngine.hpp"
 
-
-#include <unistd.h>
-
 int main(int argc, char *argv[])
 {
-    const char* cwd = getcwd(NULL,0);
-    printf("CWD: %s",cwd);
-    
     Engine.Init();
     Engine.Run();
 
