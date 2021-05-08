30cb4142b2926437c8fa6162633f0d8b768834c1
Rubberduckycooly <rubberduckycooly@outlook.com>
2021-05-08 13:29:44 +1000

    :sparkles: :sparkles: :sparkles:

--- RSDKv3/resource.h
+++ RSDKv3/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by SonicCDDecomp.rc
+// Used by RSDKv3.rc
 //
 #define IDI_ICON1                       101
 
@@ -8,7 +8,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
