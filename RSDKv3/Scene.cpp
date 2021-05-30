78cf79d8697527eb889c48dbf9c235205d87372d
Jack0b0 <81476994+Jackobo-Chocobo@users.noreply.github.com>
2021-05-30 11:31:44 +0200

    Update Scene.cpp

--- RSDKv3/Scene.cpp
+++ RSDKv3/Scene.cpp
@@ -135,7 +135,6 @@ void ProcessStage(void)
             }
             pauseEnabled      = false;
             timeEnabled       = false;
-            frameCounter      = 0;
             stageMilliseconds = 0;
             stageSeconds      = 0;
             stageMinutes      = 0;
