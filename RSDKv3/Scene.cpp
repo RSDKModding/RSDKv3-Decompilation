3b59fcbd5a420f84515d0c1a4f3454025f5dc803
Jack0b0 <81476994+Jackobo-Chocobo@users.noreply.github.com>
2021-05-30 11:36:09 +0200

    Update Scene.cpp

--- RSDKv3/Scene.cpp
+++ RSDKv3/Scene.cpp
@@ -49,6 +49,7 @@ bool pauseEnabled     = true;
 bool timeEnabled      = true;
 bool debugMode        = false;
 int frameCounter        = 0;
+int checkpoint        = false;
 int stageMilliseconds = 0;
 int stageSeconds      = 0;
 int stageMinutes      = 0;
@@ -204,7 +205,12 @@ void ProcessStage(void)
                 stageMode = STAGEMODE_PAUSED;
                 PauseSound();
             }
-
+            
+            if(checkpoint) 	{
+            	checkpoint = false;
+            	frameCounter = stageMilliseconds * Engine.refreshRate / 100;
+            }
+            
             if (timeEnabled) {
                 if (++frameCounter == Engine.refreshRate) {
                     frameCounter = 0;
