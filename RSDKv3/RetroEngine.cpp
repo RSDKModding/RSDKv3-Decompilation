585ac18afdae919a5302502b64e2aa7853e32ae4
MegAmi <angrybirdconnor@gmail.com>
2022-01-08 22:05:09 -0600

    We finally did it

--- RSDKv3/RetroEngine.cpp
+++ RSDKv3/RetroEngine.cpp
@@ -987,8 +987,7 @@ void RetroEngine::Callback(int callbackID)
             printLog("Callback: Buy Full Game Selected");
             break;
         case CALLBACK_TERMS_SELECTED: // PC = How to play, Mobile = Full Game Only Screen
-            // Uncomment when Hi Res mode is added
-            /*if (bytecodeMode == BYTECODE_PC) {
+            if (bytecodeMode == BYTECODE_PC) {
                 for (int s = 0; s < stageListCount[STAGELIST_PRESENTATION]; ++s) {
                     if (StrComp("HELP", stageList[STAGELIST_PRESENTATION][s].name)) {
                         activeStageList   = STAGELIST_PRESENTATION;
@@ -996,7 +995,7 @@ void RetroEngine::Callback(int callbackID)
                         stageMode         = STAGEMODE_LOAD;
                     }
                 }
-            }*/
+            }
             printLog("Callback: PC = How to play Menu, Mobile = Terms & Conditions Screen");
             break;
         case CALLBACK_PRIVACY_SELECTED: // PC = Controls, Mobile = Full Game Only Screen
