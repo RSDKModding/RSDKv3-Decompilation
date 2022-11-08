388df6c230941ed83c200bc11afc4dd940d4a4d0
someone <40119497+some100@users.noreply.github.com>
2022-11-07 21:26:03 -0500

    Update main.cpp

--- RSDKv3/main.cpp
+++ RSDKv3/main.cpp
@@ -60,9 +60,7 @@ int main(int argc, char *argv[])
     #if RETRO_PLATFORM == RETRO_WEB
     emscripten_set_main_loop(Run, 0, 1);
     #else
-    while (Engine.running) {
-        Run();
-    }
+    Engine.Run();
     #endif
     Engine.Quit();
 
