f317c186bcb440e4b460cfb2ca61b9b1ab7010ce
MegAmi <angrybirdconnor@gmail.com>
2023-12-29 12:26:02 -0600

    Fix fcaseopen.c buffer overflow
    Should fix #232

--- RSDKv3/fcaseopen.c
+++ RSDKv3/fcaseopen.c
@@ -30,7 +30,7 @@ THE SOFTWARE.
 #include <errno.h>
 #include <unistd.h>
 
-// r must have strlen(path) + 2 bytes
+// r must have strlen(path) + 3 bytes
 static int casepath(char const *path, char *r)
 {
     size_t l = strlen(path);
@@ -101,7 +101,7 @@ FILE *fcaseopen(char const *path, char const *mode)
     FILE *f = fopen(path, mode);
 #if !defined(_WIN32)
     if (!f) {
-        char *r = (char *)alloca(strlen(path) + 2);
+        char *r = (char *)alloca(strlen(path) + 3);
         if (casepath(path, r)) {
             f = fopen(r, mode);
         }
@@ -113,7 +113,7 @@ FILE *fcaseopen(char const *path, char const *mode)
 void casechdir(char const *path)
 {
 #if !defined(_WIN32)
-    char *r = (char *)alloca(strlen(path) + 2);
+    char *r = (char *)alloca(strlen(path) + 3);
     if (casepath(path, r)) {
         chdir(r);
     }
